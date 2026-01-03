c01461b2978be3fc8a1550db8fc8f2f542eb4e2a
LAPTOP-HN81HS1U\Nutt <nutt.akrathitipong@gmail.com>
2026-01-04 01:38:02 +0700

    updated

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3157185463"
+    serviceWorkerVersion: "1749340685"
   }
 });
